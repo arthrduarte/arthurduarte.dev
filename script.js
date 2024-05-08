adf4296d8e1f30a20034710db70cfebd70912526
o2thur <tuiduarte@gmail.com>
2024-05-08 10:23:52 -0400

    Navbar refactoring / updated about / added project 7, footer links, script.js / started responsive design

---
+++ script.js
@@ -0,0 +1,9 @@
+const sidebar = document.querySelector('.sidebar')
+
+function showSideBar() {
+    sidebar.style.display = 'flex'
+}
+
+function closeSideBar() {
+    sidebar.style.display = 'none'
+}
