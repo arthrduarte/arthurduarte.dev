a766d570044fa7b025a17ca57ef1c1c70d34ae16
Arthur Duarte <tuiduarte@gmail.com>
2025-02-16 15:57:35 -0500

    remove old content

--- script.js
+++
@@ -1,9 +0,0 @@
-const sidebar = document.querySelector('.sidebar')
-
-function showSideBar() {
-    sidebar.style.display = 'flex'
-}
-
-function closeSideBar() {
-    sidebar.style.display = 'none'
-}
